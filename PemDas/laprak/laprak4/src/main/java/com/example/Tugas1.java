980a4dc328d4666177cfe2303286948524dae676
tenma2020 <120551033+tenma2020@users.noreply.github.com>
2024-10-30 20:59:52 +0700

    work, almost final

--- PemDas/laprak/laprak4/src/main/java/com/example/Tugas1.java
+++ PemDas/laprak/laprak4/src/main/java/com/example/Tugas1.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Tugas1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);        
-        int[] temp = new int[10];               
+        Scanner in = new Scanner(System.in);
+        int[] temp = new int[10];
         while (true) {
             System.out.println("1. Input Data");
             System.out.println("2. Lihat Data");
@@ -18,20 +18,20 @@ public class Tugas1 {
             System.out.println();
 
             switch (choice) {
-                case 1:                   
-                    for(int i = 1; i < temp.length; i++){
+                case 1:
+                    for (int i = 1; i < temp.length; i++) {
                         System.out.println("Masukkan nilai ke-" + i);
                         int val = in.nextInt();
-                        temp[i] = val;                        
+                        temp[i] = val;
                     }
                     System.out.println();
                     break;
                 case 2:
                     System.out.print("[");
-                    for(int i = 0; i < temp.length; i++){
-                        if(i != (temp.length - 1)){
+                    for (int i = 0; i < temp.length; i++) {
+                        if (i != (temp.length - 1)) {
                             System.out.print(temp[i] + ", ");
-                        } else{
+                        } else {
                             System.out.print(temp[i]);
                         }
                     }
@@ -39,16 +39,16 @@ public class Tugas1 {
                     System.out.println();
                     break;
                 case 3:
-                float avg = 0;                
-                    for(int i = 0; i < temp.length; i++){
+                    float avg = 0;
+                    for (int i = 0; i < temp.length; i++) {
                         avg += temp[i];
                     }
-                    System.out.println(avg/temp.length);
+                    System.out.println(avg / temp.length);
                     System.out.println();
                     break;
                 case 4:
                     int sum = 0;
-                    for(int i = 0; i < temp.length; i++){
+                    for (int i = 0; i < temp.length; i++) {
                         sum += temp[i];
                     }
                     System.out.println(sum);
@@ -56,8 +56,8 @@ public class Tugas1 {
                     break;
                 case 5:
                     int max = temp[0];
-                    for(int i = 1; i < temp.length; i++){
-                        if(temp[i] > max){
+                    for (int i = 1; i < temp.length; i++) {
+                        if (temp[i] > max) {
                             max = temp[i];
                         }
                     }
@@ -66,8 +66,8 @@ public class Tugas1 {
                     break;
                 case 6:
                     int min = temp[0];
-                    for(int i = 1; i < temp.length; i++){
-                        if(temp[i] < min){
+                    for (int i = 1; i < temp.length; i++) {
+                        if (temp[i] < min) {
                             max = temp[i];
                         }
                     }
@@ -75,14 +75,14 @@ public class Tugas1 {
                     System.out.println();
                     break;
                 case 7:
-                System.out.println("Thank You!");     
-                               
+                    System.out.println("Thank You!");
+
                     break;
                 default:
-                System.err.println("Input Error! Try Again.");
-                System.out.println();
+                    System.err.println("Input Error! Try Again.");
+                    System.out.println();
                     break;
-            }            
+            }
         }
     }
 }
--- PemDas/laprak/laprak4/src/main/java/com/example/Tugas2.java
+++ PemDas/laprak/laprak4/src/main/java/com/example/Tugas2.java
@@ -5,6 +5,48 @@ import java.util.Scanner;
 public class Tugas2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        String[][] dataTrain = {
+                { "Jenis", "Harga", "Diskon", "AC", "Colokkan" },
+                { "Ekonomi", "50000", "2", "Tidak Ada", "Tidak Ada" },
+                { "Bisnis", "100000", "5", "Ada", "Tidak Ada" },
+                { "Eksekutif", "2000000", "7", "Ada", "Tidak Ada" },
+                { "Pariwisata", "3000000", "10", "Ada", "Ada" }
+        };
+        System.out.println(dataTrain.length);
+        while (true) {
+            System.out.println("1. Melihat Daftar Kereta Api");
+            System.out.println("2. Melihat Daftar Kereta Api yang ada AC");
+            System.out.println("3. Melihat Daftar Kereta Api yang ada Colokan");
+            System.out.println("4. Memesan Tiket Kereta Api");
+            System.out.println("5. Melihat Pesanan Tiket");
+            System.out.println("0. Keluar");
+            System.out.println("Masukkan nilai: ");
+            int choice = in.nextInt();
+
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i < dataTrain.length; i++) {
+                        if (i != 0) {
+                            System.out.printf("%s %s%10s %s %s %s%s %s %s %s %s\n", '|', dataTrain[i][0], '|',
+                                    dataTrain[i][1], '|', dataTrain[i][2], '%', '|',
+                                    dataTrain[i][3], '|', dataTrain[i][4], '|');
+                        } else {
+                            System.out.printf("%s %s %s %s %s %s %s %s %s %s %s\n", '|', dataTrain[i][0], '|',
+                                    dataTrain[i][1], '|', dataTrain[i][2], '|',
+                                    dataTrain[i][3], '|', dataTrain[i][4], '|');
+                            System.out.println();
+                        }
+
+                    }
+                    System.out.println();
+                    break;
+                case 2:
+
+                break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
