a5a72f3e8ec60a59455f3518076ac3edd89921cc
tenma2020 <120551033+tenma2020@users.noreply.github.com>
2024-11-14 16:30:49 +0700

    add more works

--- PemDas/liveCoding/lc4/untitled/src/main/java/org/example/KaprekarNumber.java
+++ PemDas/liveCoding/lc4/untitled/src/main/java/org/example/KaprekarNumber.java
@@ -1,4 +1,59 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class KaprekarNumber {
+    static int[] numbers;
+
+    static int hitungDigit(int n) {
+        int pow = (int) Math.pow(n, 2);
+        String numStr = String.valueOf(pow);
+        return numStr.length();
+    }
+
+    static boolean checkKarpekarNumber(int n) {
+        int pow = (int) Math.pow(n, 2);
+        String numStr = String.valueOf(pow);
+        if(hitungDigit(n) > 1){
+            String s1 = numStr.substring(0, numStr.length()  - hitungDigit(n));
+            String s2 = numStr.substring(numStr.length() - hitungDigit(n));
+
+            int num1 = Integer.parseInt(s1);
+            int num2 = Integer.parseInt(s2);
+            return (num1 + num2) == pow;
+        } else {
+            return n == pow;
+        }
+    }
+
+    static void printKarpekarNumber(int[] numbers) {
+        for (int number : numbers) {
+            if (number < 0) {
+                System.out.println("Program Error");
+                System.exit(0);
+            }
+        }
+        for (int number : numbers) {
+            if (checkKarpekarNumber(number)) {
+                System.out.printf("%d adalah bilangan Kaperkar\n", number);
+            } else {
+                System.out.printf("%d bukan bilangan Kaperkar\n", number);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int input;
+        numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            hitungDigit(input = in.nextInt());
+            numbers[i] = input;
+        }
+        printKarpekarNumber(numbers);
+
+
+    }
 }
--- PemDas/liveCoding/lc4/untitled/src/main/java/org/example/NT.java
+++ PemDas/liveCoding/lc4/untitled/src/main/java/org/example/NT.java
@@ -1,4 +1,41 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class NT {
+    static double[] data;
+    static double[] input(){
+        Scanner in = new Scanner(System.in);
+        data = new double[5];
+        for (int i = 0; i < 5; i++) {
+            data[i] = in.nextInt();
+        }
+        return data;
+    }
+    static double tambah(double n){
+        return data[0]+=n;
+    }
+    static double kurang(double n){
+        return data[0]-=n;
+    }
+    static double kali(double n){
+        return data[0]*=n;
+    }
+    static double bagi(double n){
+        return data[0]/=n;
+    }
+    static double akhir(){
+        return data[0]/=2;
+    }
+    static void output(double input){
+        System.out.println(input);
+    }
+    public static void main(String[] args) {
+        double[] input = input();
+        tambah(input[1]);
+        kurang(input[2]);
+        kali(input[3]);
+        bagi(input[4]);
+        output(akhir());
+    }
 }
